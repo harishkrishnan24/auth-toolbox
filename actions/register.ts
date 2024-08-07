49403b4ae0eca2f254272103e6d351841e12caf0
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-08-07 13:03:40 +0530

    save first user to db

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 "use server";
 
-import * as z from "zod";
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import * as z from "zod";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +13,16 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Email sent!" };
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already exists" };
+  }
+
+  await db.user.create({ data: { name, email, password: hashedPassword } });
+
+  return { success: "User created!" };
 };
