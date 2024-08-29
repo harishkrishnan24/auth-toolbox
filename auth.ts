e0319c7387e07945703177737d78c07bda5eaf5c
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-08-29 17:36:28 +0530

    feat:settings page

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { getUserById } from "./data/user";
 import { db } from "./lib/db";
 import { UserRole } from "@prisma/client";
 import { getTwoFactorConfirmationByUserId } from "./data/two-factor-confirmation";
+import { getAccountByUserId } from "./data/account";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   pages: {
@@ -52,6 +53,9 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       }
       if (session.user) {
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+        session.user.name = token.name;
+        session.user.email = token.email!;
+        session.user.isOAuth = token.isOAuth as boolean;
       }
       return session;
     },
@@ -62,6 +66,11 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
       if (!existingUser) return token;
 
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
       token.role = existingUser.role;
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
       return token;
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,27 @@
+import { UserRole } from "@prisma/client";
 import * as z from "zod";
+
+export const SettingsSchema = z
+  .object({
+    name: z.optional(z.string().min(2, { message: "Name is required" })),
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+    email: z.optional(z.string().email({ message: "Email is required" })),
+    password: z.optional(
+      z.string().min(6, { message: "Minimum 6 characters" })
+    ),
+    newPassword: z.optional(
+      z.string().min(6, { message: "Minimum 6 characters" })
+    ),
+  })
+  .refine(
+    (data) => {
+      if (data.password && !data.newPassword) return false;
+      if (!data.password && data.newPassword) return false;
+      return true;
+    },
+    { message: "Passwords do not match", path: ["newPassword"] }
+  );
 
 export const LoginSchema = z.object({
   email: z.string().email({ message: "Email is required" }),
